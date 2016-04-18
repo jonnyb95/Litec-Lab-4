c69ee1d3960bd96cc9808895fd0ffa43152f2931
djlebourdais <danl88@comcast.net>
2016-04-17 20:55:04 -0400

    Update Worksheet 10.c

--- Worksheet 10.c
+++ Worksheet 10.c
@@ -1,18 +1,8 @@
-//modified 4/28/2010 RPK, 11/17/2011 RPK (return 0 when not wired correctly)
+
 //LCD Model: Devantech LCD02
 //LCD Documentation: http://www.robot-electronics.co.uk/htm/Lcd02tech.htm
-/*
-kpdlcdtestPCA.c - Keypad and LCD test program - Uses PCA0 rather than Timer 0
-Spring 2010
-Use this program to exercise the LCD display and keypad to see how they work.
-This program assumes SMB is on P0.2 & P0.3 (XBR0 = 0x05)
-
-**** It is OK to change the XBR0 setting to 0x07 to match your wiring ****
-
-Students were expected to remove Timer 0 delays from kpdlcdtest.c, the old
-version of this demo and integrate it into their Gondola s/w, resulting in
-something similar to this program.
-*/
+/* Getting the Keypad and LCD to work, using PCA0. SMB is assumed to be on P0.2 & P0.3 (XBR0 = 0x05)
+Okay to change XBR0 setting to 0x07 to match wiring */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -27,21 +17,23 @@ void Interrupt_Init(void);
 void PCA_Init(void);
 void SMB0_Init(void);
 void PCA_ISR(void) __interrupt 9;
-void wait(void);
+//void wait(void);
 void pause(void);
+char twoDigitNum(void);
+void displayInput(char); 
 
 // Global variables
 unsigned int Counts, nCounts, nOverflows;
 
-//*****************************************************************************
+//MAIN FUNCTION **************************************************************
 void main(void)
 {
     char keypad;
-    Sys_Init();     // System Initialization - MUST BE 1st EXECUTABLE STATEMENT
-    Port_Init();    // Initialize ports 2 and 3 - XBR0 set to 0x05, UART0 & SMB
-    Interrupt_Init();   // You may want to change XBR0 to match your SMB wiring
-    PCA_Init();
-    SMB0_Init();
+    char tensDigit; 
+    char onesDigit; 
+    char numAsDecimal; 
+    
+    initMethods(); 
     putchar('\r');  // Dummy write to serial port
     printf("\nStart\r\n");
 
@@ -50,24 +42,65 @@ void main(void)
 
     lcd_clear();
     lcd_print("Calibration:\nHello world!\n012_345_678:\nabc def ghij");
+    
+    
     while (1)
     {
-        keypad = read_keypad();
-        pause();    // This pauses for 1 PCA0 counter clock cycle (20ms) 
-                    // If the keypad is read too frequently (no delay), it will
-                    // lock up and stop responding. Must power down to reset.
-        if (keypad != -1)   // keypad = -1 if no key is pressed
-        {           // Note: fast read results in multiple lines on terminal
-                    // A longer delay will reduce multiple keypad reads but a
-                    // better approach is to wait for a -1 between keystrokes.
-            lcd_clear();
-            lcd_print("Your key was:\n %c,  = Hex %X", keypad, keypad);
-            printf("\n\rYour key was: %c,  = Hex %X", keypad, keypad);
-            if(keypad == 0)printf("   **Wire Connection/XBR0 Error**   ");
-        }           // A returned value of 0 (0x00) indicates wiring error
+        keypadInput = read_keypad(); 
+        if (keypadInput != -1) { //if keypad pressed
+            tensDigit = keypadInput; while(read_keypad() != -1); pause();
+            
+            do {
+                onesDigit = read_keypad();
+            } while (read_keypad() == -1)
+            while(read_keypad() != -1); pause(); 
+             
+            numAsDecimal = twoDigitNum(tensDigit, onesDigit); 
+            displayInput(numAsDecimal); 
+        }
     }
 }
-//*****************************************************************************
+
+
+
+//KEYPAD READING**************************************************************
+char twoDigitNum(char tensDigit, char onesDigit) {
+    return tensDigit*10 + onesDigit; 
+}
+
+void displayInput(char) {
+    lcd_clear(); //function included in LCD library
+    lcd_print("Your key was:\n %c = Hex %X", numAsDecimal, numAsDecimal);
+    printf("\n\rYour key was: %c = Hex %X", numAsDecimal, numAsDecimal);
+    if(keypad == 0) {
+        printf("   **Wire Connection/XBR0 Error**   ");
+    }   // A returned value of 0 (0x00) indicates wiring error*/
+}
+
+//TIMING FUNCTIONS*************************************************************
+
+void pause(void)
+{
+    nCounts = 0;
+    while (nCounts < 1);// 1 count -> (65536-PCA_START) x 12/22118400 = 20ms
+}                       // 6 counts avoids most of the repeated hits
+
+void wait(void)
+{
+    nCounts = 0;
+    while (nCounts < 50);    // 50 counts -> 50 x 20ms = 1000ms
+}
+
+
+//INITIALIZATION FUNCTIONS*****************************************************
+
+void initMethods(void) {
+    Sys_Init();     // System Initialization - MUST BE 1st EXECUTABLE STATEMENT
+    Port_Init();    // Initialize ports 2 and 3 - XBR0 set to 0x05, UART0 & SMB
+    Interrupt_Init();   // You may want to change XBR0 to match your SMB wiring
+    PCA_Init();
+    SMB0_Init();
+}
 
 void Port_Init(void)	//0x05
 {
@@ -110,16 +143,4 @@ void PCA_ISR(void) __interrupt 9
         }
      }
      else PCA0CN &= 0xC0;           // clear all other 9-type interrupts
-}
-
-void pause(void)
-{
-    nCounts = 0;
-    while (nCounts < 1);// 1 count -> (65536-PCA_START) x 12/22118400 = 20ms
-}                       // 6 counts avoids most of the repeated hits
-
-void wait(void)
-{
-    nCounts = 0;
-    while (nCounts < 50);    // 50 counts -> 50 x 20ms = 1000ms
 }
